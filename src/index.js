5981b3eed75430c70370a1432ae4517b68819126
Sieusaosieungo <thinhhanh1@gmail.com>
2020-08-07 14:16:17 +0700

    finish chat-app

---
+++ src/index.js
@@ -0,0 +1,105 @@
+const path = require('path');
+const http = require('http');
+const express = require('express');
+const socketio = require('socket.io');
+const Filter = require('bad-words');
+const {
+  generateMessage,
+  generateLocationMessage,
+} = require('./utils/messages');
+const {
+  addUser,
+  removeUser,
+  getUser,
+  getUsersInRoom,
+} = require('./utils/users');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
+
+const port = process.env.PORT || 3000;
+const publicDirectoryPath = path.join(__dirname, '../public');
+
+app.use(express.static(publicDirectoryPath));
+
+io.on('connection', (socket) => {
+  console.log('New web socket connection');
+
+  socket.on('join', (options, callback) => {
+    const { error, user } = addUser({ id: socket.id, ...options });
+
+    if (error) {
+      return callback(error);
+    }
+
+    socket.join(user.room);
+
+    socket.emit('message', generateMessage('Admin', 'Welcome!'));
+    socket.broadcast
+      .to(user.room)
+      .emit(
+        'message',
+        generateMessage('Admin', `${user.username} has joined!`)
+      );
+    io.to(user.room).emit('roomData', {
+      room: user.room,
+      users: getUsersInRoom(user.room),
+    });
+    callback();
+    //socket.emit, io.emit, socket.broadcast.emit
+    // io.to.emit, socket.broadcast.to.emit
+  });
+
+  socket.on('sendMessage', (message, callback) => {
+    const filter = new Filter();
+
+    if (filter.isProfane(message)) {
+      return callback('Profanity is not allowed!');
+    }
+
+    const user = getUser(socket.id);
+
+    io.to(user.room).emit('message', generateMessage(user.username, message));
+    callback();
+  });
+
+  socket.on('sendLocation', ({ latitude, longitude }, callback) => {
+    const user = getUser(socket.id);
+
+    io.to(user.room).emit(
+      'locationMessage',
+      generateLocationMessage(
+        user.username,
+        `https://google.com/maps?q=${latitude},${longitude}`
+      )
+    );
+    callback();
+  });
+
+  socket.on('disconnect', () => {
+    const user = removeUser(socket.id);
+
+    if (user) {
+      io.to(user.room).emit(
+        'message',
+        generateMessage('Admin', `${user.username} has left!`)
+      );
+      io.to(user.room).emit('roomData', {
+        room: user.room,
+        users: getUsersInRoom(user.room),
+      });
+    }
+  });
+
+  // socket.emit('countUpdated', count); // if using io.emit ==> everytime a new client joined, old client would get the new count ===> waste because the count hasn't changed, no need to send it to everyone
+  // socket.on('increment', () => {
+  //   count++;
+  //   // socket.emit('countUpdated', count); //specificed connection
+  //   io.emit('countUpdated', count); //every connection
+  // });
+});
+
+server.listen(port, () => {
+  console.log(`Server is up on port ${port}!`);
+});
